dddd0e9bda4a3e0d66eb097301cc99d1fb6b378d
Aaron J. Graves <cajunman4life@gmail.com>
2014-11-21 10:31:38 -0600

    aneuch.pl - Updated $VERSION and $VERSIONID to 0.50 and 0050 respectively

--- aneuch.pl
+++ aneuch.pl
@@ -54,8 +54,8 @@ my %srvr = (
   80 => 'http://',	443 => 'https://',
 );
 
-$VERSION = '0.41';	# Set version number
-$VERSIONID = '0041';	# Version ID
+$VERSION = '0.50';	# Set version number
+$VERSIONID = '0050';	# Version ID
 
 # Subs
 sub InitConfig  {
