30bc4e1af4399ffd41c8e4f63348eba06dd29050
Aaron J. Graves <cajunman4life@gmail.com>
2017-10-31 17:15:45 -0500

    Re-written to use the preferences API

--- plugins/Snippets.pl
+++ plugins/Snippets.pl
@@ -6,57 +6,46 @@
 package Aneuch;
 return unless $Aneuch::VERSIONID >= '0060';	# Require 0.60 or higher.
 RegPlugin('Snippets.pl', 'publish snippets of code for your wiki');
-my $SnippetDB = "$DataDir/snippets";
 
 sub DoSnippet {
   my $code = shift;
   $code = ReplaceSpaces($code);
-  my %db = ReadDB($SnippetDB);
-  if(defined $db{$code}) {
-    return $db{$code};
-  } else {
-    return '';
-  }
+  return GetPrefs('Snippets',$code);
 }
 
 sub DoAdminSnippet {
   my $snippet = GetParam('snippet');
   if($snippet) {	# Editing a snippet
     if($snippet eq 'NEW') { $snippet = ''; }
-    my %f = ReadDB($SnippetDB);
+    my $content = ($snippet) ? GetPrefs('Snippets',$snippet) : '';
     print Form('snippet','post', '',
       $q->div({-class=>'form-group'},
 	$q->label({-for=>'snippetname'},"Name: "),
 	$q->textfield(-name=>'snippetname', -size=>40, -default=>$snippet,
-	  -class=>'form-control')
+	  -class=>'form-control', -placeholder=>'Name your snippet')
       ),
       $q->div({-class=>'form-group'},
 	$q->label({-for=>'snippetcode'},
 	  "Code: (to delete the snippet, clear out the code and save)"),
 	$q->textarea(-name=>'snippetcode', -columns=>80, -rows=>20,
-	  -default=>$f{$snippet}, -class=>'form-control')
+	  -default=>$content, -class=>'form-control', 
+	  -placeholder=>'Snippet contents')
       ),
       $q->submit(-class=>'btn btn-success', -value=>'Save'), " ",
-      #AdminLink('snippet',"Back to snippets menu")
       $q->input({-type=>'button', -onClick=>
-	"location.href='$Url?do=admin;page=snippet'", 
+	'location.href="'.AdminURL('snippet').'"', 
 	-class=>'btn btn-primary', -value=>'Return to menu'})
     );
     return;
   }
   # Otherwise, main interface
   print $q->p("Here are your snippets:");
-  my %db = ReadDB($SnippetDB);
-  #print "<ul>";
+  my @snippets = GetPrefs('Snippets');
   print '<div class="list-group">';
-  foreach $snip (sort keys %db) {
-    #print "<li><a href=\"$ShortURL?do=admin;page=snippet;snippet=$snip\">".
-    #  "$snip</a></li>";
-    #print $q->li(AdminLink('snippet',$snip,"snippet=$snip"));
+  foreach $snip (sort @snippets) {
     print $q->a({-href=>$Url."?do=admin;page=snippet;snippet=$snip",
       -class=>'list-group-item'}, $snip);
   }
-  #print "</ul>";
   print '</div>';
   print $q->button(-value=>'New Snippet', -class=>'btn btn-success',
     -onClick=>"location.href='$ShortUrl?do=admin;page=snippet;snippet=NEW'");
@@ -67,25 +56,16 @@ sub DoPostingSnippet {
     my $name = ReplaceSpaces(GetParam('snippetname'));
     if(Trim($name) ne '') {
       my $code = UnquoteHTML(GetParam('snippetcode'));
-      my %f = ReadDB($SnippetDB);
       if($code eq '') {
-	delete $f{$name};
+	DelPrefs('Snippets',$name);
       } else {
-	$f{$name} = $code;
+	SetPrefs('Snippets',$name,$code);
       }
-      WriteDB($SnippetDB, \%f);
     }
   }
-  ReDirect($Url."?do=admin;page=snippet");
-}
-
-sub InitSnippetPlugin {
-  if(!-f "$SnippetDB") {
-    StringToFile('',$SnippetDB);
-  }
+  ReDirect(AdminURL('snippet'));
 }
 
 RegShortCode('snippet', \&DoSnippet);
 RegAdminPage('snippet', 'Manage snippets', \&DoAdminSnippet);
 RegPostAction('snippet', \&DoPostingSnippet);
-InitSnippetPlugin();
